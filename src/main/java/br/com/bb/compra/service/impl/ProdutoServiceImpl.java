0807490d03dab26a3c860a1e3095bb76e8ac098b
juliano.cervelin <juliano.cervelin@gmail.com>
2022-08-31 20:01:18 +0100

    Produto converters

--- src/main/java/br/com/bb/compra/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/bb/compra/service/impl/ProdutoServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.bb.compra.converter.ProdutoConverter;
 import br.com.bb.compra.model.Produto;
 import br.com.bb.compra.model.entity.ProdutoEntity;
 import br.com.bb.compra.repository.ProdutoRepository;
+import br.com.bb.compra.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,28 +13,32 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ProdutoServiceImpl {
+public class ProdutoServiceImpl implements ProdutoService {
 
     private final ProdutoRepository repository;
 
+    @Override
     public Produto salvar(Produto novoProduto) {
         final ProdutoEntity produtoEntity = ProdutoConverter.convertProductTo(novoProduto);
         final ProdutoEntity produtoSalvo = repository.save(produtoEntity);
         return ProdutoConverter.convertEntityTo(produtoSalvo);
     }
 
+    @Override
     public List<Produto> salvarLista(List<Produto> novoProduto) {
         final List<ProdutoEntity> produtoEntityList = ProdutoConverter.convertProductTo(novoProduto);
         repository.saveAll(produtoEntityList);
         return ProdutoConverter.convertEntityTo(produtoEntityList);
     }
 
+    @Override
     public Produto buscaPorId(Long id) {
         final ProdutoEntity produtoSalvo = repository.findById(id)
                 .orElseThrow(RuntimeException::new);
         return ProdutoConverter.convertEntityTo(produtoSalvo);
     }
 
+    @Override
     @Transactional(readOnly = true)
     public boolean isEmpty() {
         return repository.count() == 0;
